8f59430f89f7c355f1135bb64185f44c2362b08f
vitaly <black-lite21@yandex.ru>
2026-05-15 07:09:08 +0700

    Добавил Laravel Breeze c React + Inertia + PHPUnit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+    server: {
+        host: '0.0.0.0',
+        hmr: { host: '127.0.0.1' }
+    },
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: 'resources/js/app.tsx',
             refresh: true,
         }),
+        react(),
     ],
 });
